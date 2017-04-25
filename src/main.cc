35d04b00b82e6cdb54bbbeebbaa292c467d5359f
Brendan Goodenough <brendan@goodenough.nz>
2017-04-25 16:49:40 +1200

    Create window and keep open until quit

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,70 @@
+#include <SDL2/SDL.h>
 #include <stdio.h>
 
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+bool init();
+void gameloop();
+void close();
+
+SDL_Window *gWindow = NULL;
+SDL_Renderer *gRenderer = NULL;
+
+bool init() {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("SDL failed to initialise! SDL Error: %s\n", SDL_GetError());
+    return false;
+  }
+
+  gWindow = SDL_CreateWindow("Tower Defense", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (gWindow == NULL) {
+    printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+    return false;
+  }
+
+  gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (gRenderer == NULL) {
+    printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+    return false;
+  }
+
+  return true;
+}
+
+void gameloop() {
+  bool running = true;
+  SDL_Event e;
+
+  while (running) {
+    while (SDL_PollEvent(&e) != 0) {
+      if (e.type == SDL_QUIT) {
+        running = false;
+      }
+    }
+
+    // Do stuff
+  }
+}
+
+void close() {
+  SDL_DestroyRenderer(gRenderer);
+  gRenderer = NULL;
+
+  SDL_DestroyWindow(gWindow);
+  gWindow = NULL;
+
+  SDL_Quit();
+}
+
 int main(int argc, char *args[]) {
-  printf("Hello, world!\n");
+  if (!init()) {
+    printf("Initialise failed!\n");
+    return 0;
+  }
+
+  gameloop();
+  close();
+
   return 0;
 }
